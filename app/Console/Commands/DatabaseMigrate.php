d9ef4bdff94f79aa0dabd18c5eee30bdab3810d0
t_inoue <t.artwork.ceramica@gmail.com>
2024-01-28 00:32:58 +0900

    add: enum setting and command
    - artisan コマンドの作成
    - enum 管理のライブラリを作成

---
+++ app/Console/Commands/DatabaseMigrate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DatabaseMigrate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:db-migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Execute the process of migrating the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Hello, World!');
+    }
+}
