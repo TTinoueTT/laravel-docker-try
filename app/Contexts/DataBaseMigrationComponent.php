82df1e73e76531aa190a69d41412d3560270ffa9
t_inoue <t.artwork.ceramica@gmail.com>
2024-01-28 22:53:23 +0900

    add: user entity
    - DB の疎通確認、メソッドを利用したentity 操作を確認

--- app/Contexts/DataBaseMigrationComponent.php
+++ app/Contexts/DataBaseMigrationComponent.php
@@ -2,18 +2,37 @@
 
 namespace App\Contexts;
 
+use App\Models\OldUser;
+use App\Models\OldUserData;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Collection;
 
 class DataBaseMigrationComponent
 {
     public static function migrate_exec(): void
     {
         Log::info("start database migrate execution");
+
+        // OldUser::chunk(200, function (Collection $users) {
+        //     foreach ($users as $user) {
+        //         // ...
+        $user = OldUser::find(1);
+
+        # profile の取得と、target_profiles の取得
+        # histories の取得
+        //     }
+        // });
+
         // DB に接続
-        $array = DB::connection('mysql_old')->select('select * from users_data');
+        // $array = OldUser::all();
+        // $array = OldUserData::all();
+        $userData = OldUserData::find(3);
+        // $array = DB::connection('mysql_old')->select('select * from users_data');
         // DB::connection('mysql_old')->select('select * from users');
-        // users テーブルの一覧を取得して、
-        Log::info($array);
+        # ① users テーブルの一覧を取得
+        # ② users に関与するテーブルごとに新規DBにインサート処理を行う
+        # たとえば、
+        Log::info($userData->user_id);
     }
 }
---
+++ app/Models/OldUser.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OldUser extends Model
+{
+    use HasFactory;
+
+    /**
+     * このモデルが使用するデータベース接続
+     *
+     * @var string
+     */
+    protected $connection = 'mysql_old';
+
+    /**
+     * モデルに関連付けるテーブル、tableプロパティを定義してモデルのテーブル名を自分で指定できる
+     * 別の名前を明示的に指定しない限り、クラスの複数形の「スネークケース」をテーブル名として使用
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * モデルにタイムスタンプを付けるか
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * モデルの属性のデフォルト値
+     *
+     * @var array
+     */
+    protected $attributes = [
+        self::EMAIL => 0,
+        self::ENCRYPTED_PASSWORD => '[]',
+        self::REF_FROM => '[]',
+        self::REF_CATE => '[]',
+        self::REF_U_ID => '[]',
+        self::REF_AD_ID => '[]',
+        self::MIGRATION_CODE => '[]',
+        self::INTENT => '[]',
+        self::MAIL_ADDRESS => '[]',
+        self::NOTIFICATION => '[]',
+        self::NOTIFICATION_OPTOUT_AT => '[]',
+        self::NOTIFICATION_OPTIN_AT => '[]',
+    ];
+
+    const EMAIL = "email";
+    const ENCRYPTED_PASSWORD = "encrypted_password";
+    const REF_FROM = "ref_from";
+    const REF_CATE = "ref_cate";
+    const REF_U_ID = "ref_u_id";
+    const SIGN_IN_COUNT = "sign_in_count";
+    const REF_AD_ID = "ref_ad_id";
+    const MIGRATION_CODE = "migration_code";
+    const INTENT = "intent";
+    const MAIL_ADDRESS = "mail_address";
+    const NOTIFICATION = "notification";
+    const NOTIFICATION_OPTOUT_AT = "notification_optout_at";
+    const NOTIFICATION_OPTIN_AT = "notification_optin_at";
+}
