16d046411966adfc26f867b62311a258dc91c8e2
t_inoue <t.artwork.ceramica@gmail.com>
2024-02-13 17:32:27 +0900

    roop id is desc

--- app/Contexts/DataBaseMigrationComponent.php
+++ app/Contexts/DataBaseMigrationComponent.php
@@ -45,16 +45,51 @@ class DataBaseMigrationComponent
 
     public function migrate_exec(): void
     {
-        $repeatTime = 200;
+
+
+        Log::info("___________________________________________________________________________");
+        Log::info("        /    /                   ,");
+        Log::info("----__-/----/__---------_--_----------__----)__-----__---_/_-----__----)__-");
+        Log::info("  /   /    /   ) ===   / /  )  /    /   )  /   )  /   )  /     /   )  /   )");
+        Log::info("_(___/____(___/_______/_/__/__/____(___/__/______(___(__(_ ___(___/__/_____");
+        Log::info("                                      /");
+        Log::info("                                  (_ /");
+
+        //    ______
+        //   / ____/___  ____ ___  ____  ____  ________  _____
+        //  / /   / __ \\/ __ `__ \\/ __ \\/ __ \\/ ___/ _ \\/ ___/
+        // / /___/ /_/ / / / / / / /_/ / /_/ (__  )  __/ /
+        // \\____/\\____/_/ /_/ /_/ .___/\\____/____/\\___/_/
+
+        // Log::info("          __  __                           _                   _");
+        // Log::info("         / / / /                          (_)                   | |");
+        // Log::info("   _____/ / / /_              __ __  __   __ _  _ __   __ _ | |_   ___   _ __");
+        // Log::info("  / __ ` / / __  \   ______  / /_  `_  \ / /__ _` || '__| / _` || __| / _ \ | '__|");
+        // Log::info(" / /  / / / /  | | /______/ / ' / / / / / // _` || '__| / _` || __| / _ \ | '__|");
+        // Log::info("| /__/ / / /__/ /          / / / / / / / // (_| || |   | (_| || |_ | (_) || |");
+        // Log::info(" \__,_/ /_.____/          /_/ /_/ /_/ /_/ \__, ||_|    \__,_| \__| \___/ |_|");
+        // Log::info("                                               __/ |");
+        // Log::info("                                              |___/");
+        // Log::info("         _      _                         _                      _");
+        // Log::info("       /  /   / /                       (_)                    | |");
+        // Log::info("   __ /  /   / /__   ______  _ __ ___   _   __ _  _ __   __ _ | |_   ___   _ __");
+        // Log::info(" / _ `  /   /     '_ \ |______|| '_ ` _ \ | | / _` || '__| / _` || __| / _ \ | '__|");
+        // Log::info("| (_/  /   / /_) /       | | | | | || || (_| || |   | (_| || |_ | (_) || |");
+        // Log::info(" \__,_/   /_.__/         |_| |_| |_||_| \__, ||_|    \__,_| \__| \___/ |_|");
+        // Log::info("                                      __/ |");
+        // Log::info("                                     |___/");
+        Log::info("*************************************************************");
+        Log::info("START DATABASE MIGRATE EXECUTION");
+
+        $repeatTime = 2;
         $counter = 0;
-        Log::info("start database migrate execution");
 
         DB::connection('mysql_old')->beginTransaction();
         DB::connection('mysql_new')->beginTransaction();
         DB::connection('mysql_new_payment')->beginTransaction();
 
         try {
-            OldUser::chunk($repeatTime, function (Collection $oldUsers) use ($repeatTime, $counter) {
+            OldUser::orderBy('id', 'desc')->chunk($repeatTime, function (Collection $oldUsers) use ($repeatTime, $counter) {
                 // 処理回数を追跡するカウンタ
                 foreach ($oldUsers as $oldUser) {
 
@@ -67,13 +102,22 @@ class DataBaseMigrationComponent
                     * => 190 Softbank
                     * => 22, 21 Rakuten
                     */
-                    if ($oldUser->id != 22) {
-                        continue;
-                    }
+                    // if ($oldUser->id != 190) {
+                    //     continue;
+                    // }
 
                     # users レコードの移行(決済継続データの移行も)
-                    Log::info("users id: {$oldUser->id}");
+                    Log::info("*************************************************************");
+                    Log::info("Start migrate user ((( id: {$oldUser->id} )))");
+                    Log::info("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+
                     $nextUser = $this->userService->migrateOldToNew($oldUser);
+                    if ($nextUser == null) {
+                        Log::info("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
+                        Log::info("New user is null, old user ((( id :{$oldUser->id} ))) has invalid parameter");
+                        Log::info("*************************************************************");
+                        continue;
+                    }
 
                     # TODO profile の移行(旧profile情報の重複も考慮)
                     $migrateProfileIdMap = $this->profileService->migrateOldToNewWithNew($oldUser, $nextUser);
@@ -90,6 +134,9 @@ class DataBaseMigrationComponent
                     # userAnalysis の移行
                     $this->userAnalysisService->migrateOldToNewWithNew($oldUser, $nextUser);
 
+                    Log::info("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
+                    Log::info("old user ((( id :{$oldUser->id} ))) => migrate to new user ((( id: {$nextUser->id} )))");
+                    Log::info("*************************************************************");
 
                     Log::info("======#{$counter}");
 
@@ -132,6 +179,5 @@ class DataBaseMigrationComponent
         # ① users テーブルの一覧を取得
         # ② users に関与するテーブルごとに新規DBにインサート処理を行う
         # たとえば、
-        // Log::info($userData->user_id);
     }
 }
