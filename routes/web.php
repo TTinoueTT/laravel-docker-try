fbc0b7c2c60f09587a58b11ecfa434a3ec835da9
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-18 22:04:15 +0900

    ルーティングとコントローラ: グループ化

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,10 @@ Route::get('/', function () {
 Route::get('messages', [MessageController::class, 'index']);
 Route::post('messages', [MessageController::class, 'store']);
 
-Route::get('admin/books', [BookController::class, 'index'])->name('book.index');
-Route::get('admin/books/{id}', [BookController::class, 'show'])->whereNumber('id')->name('book.show');
+Route::prefix('admin/books')
+    ->name('book.')
+    ->controller(BookController::class)
+    ->group(function () {
+        Route::get('', 'index')->name('index');
+        Route::get('{id}', 'show')->whereNumber('id')->name('show');
+    });
