fbbb64019bdc18d65c70eaae0bfee8e9be287331
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-18 14:27:41 +0900

    fix(Author, AuthorDetail): Author to AuthorDetail
    - 1:1 の関連

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Models\AuthorDetail;
 
 class Author extends Model
 {
     use HasFactory;
+
+    public function detail(): HasOne
+    {
+        return $this->hasOne(AuthorDetail::class);
+    }
 
     public function books(): BelongsToMany
     {
