361f1d41ad85bb6d207e939e4c785f93b27a3679
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-19 14:53:30 +0900

    コンポーネント: error-message.blade のリファクタリング

--- resources/views/components/error-messages.blade.php
+++ resources/views/components/error-messages.blade.php
@@ -1,16 +1,11 @@
 <div style="color:red">
     <ul>
-        @foreach ($errors->all() as $error)
+        @foreach ($errors->take(2) as $error)
             <li>{{ $error }}</li>
-            {{-- blade-formatter-disable --}}
-            @if ($loop->iteration >= 2)
-                @break
-            @endif
-            {{-- blade-formatter-enable --}}
         @endforeach
 
         @if ($has2MoreErrors())
-            ...以下略
+            <li>...以下略</li>
         @endif
     </ul>
 </div>
