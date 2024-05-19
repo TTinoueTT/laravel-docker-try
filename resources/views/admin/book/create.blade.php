2f402c62bfdc999e669e13408601476211c82694
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-19 16:19:49 +0900

    コンポーネント: レイアウトコンポーネント

--- resources/views/admin/book/create.blade.php
+++ resources/views/admin/book/create.blade.php
@@ -1,45 +1,38 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>書籍登録</title>
-</head>
-
-<body>
-    <main>
-        <h1>書籍登録</h1>
-        {{-- リダイレクト時のエラー文 --}}
-        @if ($errors->any())
-            <x-alert>
-                <x-error-messages :$errors />
-            </x-alert>
-        @endif
-        <form action="{{ route('admin.book.store') }}" method="post">
-            @csrf
-            <div>
-                <label for="category">カテゴリ</label>
-                <select name="category_id" id="category">
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}" @selected(old('category_id', $category->title == 'management' ? $category->id : null) == $category->id)>
-                            {{ $category->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label for="title">タイトル</label>
-                <input type="text" name="title" id="title" value="{{ old('title') }}">
-            </div>
-            <div>
-                <label for="price">価格</label>
-                <input type="text" name="price" id="price" value="{{ old('price') }}">
-            </div>
-            <input type="submit" value="送信">
-        </form>
-    </main>
-</body>
-
-</html>
+<x-layouts.book-manager>
+    <x-slot:title>
+        {{--
+            slot で呼び出されるコンポーネント側から
+            layouts 側に渡す変数の値を指定
+        --}}
+        書籍登録
+    </x-slot:title>
+    <h1>書籍登録</h1>
+    {{-- リダイレクト時のエラー文 --}}
+    @if ($errors->any())
+        <x-alert>
+            <x-error-messages :$errors />
+        </x-alert>
+    @endif
+    <form action="{{ route('admin.book.store') }}" method="post">
+        @csrf
+        <div>
+            <label for="category">カテゴリ</label>
+            <select name="category_id" id="category">
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}" @selected(old('category_id', $category->title == 'management' ? $category->id : null) == $category->id)>
+                        {{ $category->title }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <label for="title">タイトル</label>
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
+        </div>
+        <div>
+            <label for="price">価格</label>
+            <input type="text" name="price" id="price" value="{{ old('price') }}">
+        </div>
+        <input type="submit" value="送信">
+    </form>
+</x-layouts.book-manager>
--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>書籍一覧</title>
-</head>
-
-<body>
-    <main>
-        <h1>書籍一覧</h1>
-        @if (session('message'))
-            <div style="color: blue">
-                {{ session('message') }}
-            </div>
-        @endif
-        <a href="{{ route('admin.book.create') }}">追加</a>
-        <x-book-table :$books />
-
-    </main>
-</body>
-
-</html>
+<x-layouts.book-manager>
+    <x-slot:title>
+        {{--
+            slot で呼び出されるコンポーネント側から
+            layouts 側に渡す変数の値を指定
+        --}}
+        書籍一覧
+    </x-slot:title>
+    <h1>書籍一覧</h1>
+    @if (session('message'))
+        <div style="color: blue">
+            {{ session('message') }}
+        </div>
+    @endif
+    <a href="{{ route('admin.book.create') }}">追加</a>
+    <x-book-table :$books />
+</x-layouts.book-manager>
