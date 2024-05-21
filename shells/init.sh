c422b5489b8079b61fbfbe07ea43ffc5b4eadf38
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-21 21:06:30 +0900

    ミドルウェアを利用したアクセスログ
    - AccessLogger ミドルウェアを作成する
    - ミドルウェアの登録

---
+++ shells/init.sh
@@ -0,0 +1,4 @@
+docker compose exec app php artisan config:cache
+docker compose exec app php artisan config:clear
+docker compose exec app php artisan route:clear
+docker compose exec app php artisan view:clear
