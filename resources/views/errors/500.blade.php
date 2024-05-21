88db6d5b5b462548311f6278af385098b3f5f57a
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-21 19:18:37 +0900

    エラーハンドリング: HTTPリクエストエラーとアプリケーションエラー

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>500 Application error</title>
+</head>
+
+<body>
+    <h1>500 Application error</h1>
+    <p>エラーが発生しました。時間をおいて再度お試しください。</p>
+</body>
+
+</html>
