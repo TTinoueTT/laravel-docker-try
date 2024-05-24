bfde10e66c8624b0d52b9021249451fdd7df73b4
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-24 22:04:31 +0900

    リソースによるJSON作成

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -3,24 +3,26 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MessageResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Models\Message;
 
 
 class MessageController extends Controller
 {
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
         $messages = Message::all();
 
-        return $messages;
+        return MessageResource::collection($messages);
     }
 
-    public function show(Message $message): Message
+    public function show(Message $message): MessageResource
     {
-        return $message;
+        return new MessageResource($message);
     }
 
     public function store(Request $request): Message
