34164942bbcd96f26ca58fd63500c2c8e1ca5924
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-24 14:21:12 +0900

    ユニットテスト： ユニットテストを作成から実行

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
@@ -19,5 +20,11 @@ class Book extends Model
     public function authors(): BelongsToMany
     {
         return $this->belongsToMany(Author::class)->withTimestamps();
+    }
+
+    public function abbreviatedTitle(): string
+    {
+        // タイトルが11文字以上のときは省略する(11文字以降は '...' になる。)
+        return Str::limit($this->title, 10, '...');
     }
 }
