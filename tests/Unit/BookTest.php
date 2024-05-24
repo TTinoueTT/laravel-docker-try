de569d2d496e22556beec78456775f50d6366f8e
t_inoue <t.artwork.ceramica@gmail.com>
2024-05-24 14:33:05 +0900

    ユニットテスト: ユニットテストをフィーチャーテストとして呼ぶ

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use App\Models\Book;
 
 class BookTest extends TestCase
@@ -13,12 +14,14 @@ class BookTest extends TestCase
     /** @test */
     public function 書籍のタイトルが11文字で省略される(): void
     {
-        $book1 = new Book();
-        $book1->title = 'PHPer Book'; // 10文字
+        // $book1 = new Book();
+        // $book1->title = 'PHPer Book'; // 10文字
+        $book1 = Book::factory()->make(['title' => 'PHPer Book']);
         $this->assertSame('PHPer Book', $book1->abbreviatedTitle());
 
-        $book2 = new Book();
-        $book2->title = 'PHPer Book2'; // 11文字
+        // $book2 = new Book();
+        // $book2->title = 'PHPer Book2'; // 11文字
+        $book2 = Book::factory()->make(['title' => 'PHPer Book2']);
         $this->assertSame('PHPer Book...', $book2->abbreviatedTitle());
     }
 }
